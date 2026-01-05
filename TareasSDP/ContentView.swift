9070415023f30848f704341ec5a755f3df3334a6
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2026-01-05 12:58:09 +0100

    Initial Commit

---
+++ TareasSDP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TareasSDP
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 5/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TareasSDP/TareasSDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TareasSDPApp.swift
+//  TareasSDP
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TareasSDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
